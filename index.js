62c7bd9670ce19a74470e7060710cd412246c1b5
OZNONO <jiho3541@naver.con>
2020-05-02 23:26:07 +0900

    20200502 - 3
    prac template

---
+++ index.js
@@ -0,0 +1,25 @@
+jQuery(".nav-link").hover( function () { jQuery(this).addClass("active"); }, function () { jQuery(this).removeClass("active"); } );
+
+function loadran() {
+ $("#bonmoon").empty();
+ bonmoon.innerHTML = "<p>대문이 아니다</p>\
+ <a href=\"./recommend20.html\" class=\"btn btn-default border\">20레벨 무작위 곡 추천</a><br><br>\
+ <a href=\"./recommend19.html\" class=\"btn btn-secondary\">19레벨 무작위 곡 추천</a><br><br>\
+ <a href=\"./recommend18.html\" class=\"btn btn-default border\">18레벨 무작위 곡 추천</a><br><br>\
+ <a href=\"./recommend17.html\" class=\"btn btn-secondary border\">17레벨 무작위 곡 추천</a><br><br>\
+ <hr>\
+ <a href=\"./recommend1718.html\" class=\"btn btn-default border\">17~18레벨 무작위 곡 추천</a><br><br>";
+};
+
+
+function loadrec() {
+   $("#bonmoon").empty();
+   bonmoon.innerHTML = "<p>엄선된? 추천곡들을 모았습니다.</p>\
+  <a href=\"./rec18.html\" class=\"btn btn-default border\">엄선된 18레벨 곡 추천</a><br><br>\
+  <a href=\"./rec17.html\" class=\"btn btn-secondary border\">엄선된 17레벨 곡 추천</a><br><br>"
+};
+
+function loadmemo() {
+   $("#bonmoon").empty();
+   bonmoon.innerHTML = "<p>asdf</p>"
+}
